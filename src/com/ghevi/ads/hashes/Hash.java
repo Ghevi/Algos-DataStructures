fec63b58e416cb538fcf699c429691f885db95eb
Ghevi <ghevi96@gmail.com>
2020-05-02 20:14:53 +0200

    new file:   Notes/Hashes 	new file:   out/production/Algos&DataStructures/com/ghevi/ads/hashes/Hash.class 	new file:   out/production/Algos&DataStructures/com/ghevi/ads/linkedlists/DoublyLinkedList$IteratorHelper.class 	new file:   out/production/Algos&DataStructures/com/ghevi/ads/linkedlists/DoublyLinkedList$Node.class 	new file:   out/production/Algos&DataStructures/com/ghevi/ads/linkedlists/DoublyLinkedList.class 	modified:   out/production/Algos&DataStructures/com/ghevi/ads/linkedlists/LinkedList.class 	new file:   src/com/ghevi/ads/hashes/Hash.java

---
+++ src/com/ghevi/ads/hashes/Hash.java
@@ -0,0 +1,32 @@
+package com.ghevi.ads.hashes;
+
+public class Hash {
+
+    public static void main(String[] args) {
+
+        String s = "my name is Josh";
+
+        char c = s.charAt(6); // e
+
+        int i = s.charAt(6); // unicode value 101
+
+        System.out.println(i);
+
+        // ----
+
+        int hashVal = hashCode(s);
+    }
+
+    public static int hashCode(String s){
+        int g = 31; //
+        int hash = 0;
+
+        // permutation
+        for(int i = 0; i < s.length(); i++){
+            hash = g * hash + s.charAt(i);
+        }
+
+        return hash;
+    }
+
+}
